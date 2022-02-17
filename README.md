9208da8b5258e36bb1061d7e880a63b8e7c80a7e
Lukáš Drahník <ldrahnik@gmail.com>
2022-02-17 03:31:54 +0000

    Init

---
+++ README.md
@@ -0,0 +1,27 @@
+# PRO OC DZS Proxy
+
+## Použití
+
+GET <serverUrl>/dotaz.nsf?kdo=<Kmenove čislo>&dD=<Den narození např. 19>&dM=<Měsíc narození např. 05>&dR=<Rok narození např. 1994>
+
+Péči hradí MZČR
+```
+<font color="#FFFFFF">&gt;</font>
+<b>Jméno přijmení</b>
+<br>
+<font color="#FFFFFF">&gt;</font>
+<font size="2">student momentálně </font>
+<font size="2" color="#008000">STUDUJE</font>
+<font size="2">, péči hradí MZČR.</font>
+```
+ 
+Nenalezeno
+```
+<font color="#FFFFFF">&gt;</font>
+<b>Nenalezeno</b>
+<br>
+<font color="#FFFFFF">&gt;</font>
+<font size="2">student momentálně </font>
+<font size="2" color="#FF0000">NESTUDUJE</font>
+<font size="2">, péči hradí jako samoplátce.</font>
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+var express = require('express');
+const puppeteer = require('puppeteer');
+var app = express();
+
+async function getVysledekKontroly(kdo, dD, dM, dR, onSuccess) {
+
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox','--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('http://registr.dzs.cz/dotaz.nsf/');
+    await page.waitForSelector('input[type="button"]');
+    await page.click('input[type="button"]');
+    await page.waitForSelector('input[type="button"]');
+    await page.type('input[name=kdo]', kdo);
+    await page.type('input[name=dD]', dD);
+    await page.type('input[name=dM]', dM);
+    await page.type('input[name=dR]', dR);
+    await page.click('input[type="button"]');
+
+    await page.waitForSelector('form tr:last-child td:last-child');
+    const Vysledek = await page.evaluate(() => {
+        const anchor = document.querySelector('form tr:last-child td:last-child');
+        return anchor.innerHTML;
+    });
+    onSuccess(Vysledek);
+}
+
+app.use(function(req, res, next) {
+
+    console.log(req.method + ': new request');
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'X-Requested-With');
+
+    if (req.method === 'OPTIONS') {
+        console.log('OPTIONS: preflight sent');
+        res.send();
+    } else if (req.method === "GET") {
+
+        if(req.baseUrl + req.path == "/dotaz.nsf") {
+            return new Promise(resolve => {
+
+                const kdo = req.query.kdo;
+                const dD = req.query.dD;
+                const dM = req.query.dM;
+                const dR = req.query.dR;
+
+                if(!kdo || !dD || !dM || !dR) {
+                    return;
+                }
+
+                getVysledekKontroly(kdo, dD, dM, dR, function(Vysledek) {
+                    console.log(Vysledek);
+                    resolve(Vysledek);
+                });
+            }).then(body => {
+                console.log('GET: response sent');
+                res.send(body);
+            });
+        }
+    } else {
+        console.log(req.method + ': is not GET or OPTION request');
+        next();
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
